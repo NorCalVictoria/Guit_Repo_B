0b94d753575fea37ae319869efad837a4af82331
V2E <ENGINE@Engine.local>
2020-04-23 11:05:38 -0700

    initial

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ENGINE/src/ALL_PROJ/2website_aff/env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ projwsite/aff/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AffConfig(AppConfig):
+    name = 'aff'
