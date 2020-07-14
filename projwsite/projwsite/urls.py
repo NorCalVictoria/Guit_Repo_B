0d8158490098b8498bc1d8a40ab185ee8ef43363
V2E <ENGINE@V-Engine.local>
2020-07-14 01:26:19 -0700

    about

--- projwsite/projwsite/urls.py
+++ projwsite/projwsite/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include, reverse
+from aff import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('aff.urls'))
+    path('', include('aff.urls')),
+    path('', views.home, name='home'),
+	path('about/', views.about, name='about'),
+	path('blog/', views.blog, name='blog'),
+	path('covers/', views.covers, name='covers'),
 ]
