bde9a3168024af065c8c02e3cfd2b8ff065cdedb
V2E <ENGINE@V-Engine.local>
2020-07-13 23:23:29 -0700

    about

--- projwsite/aff/views.py
+++ projwsite/aff/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return render(request, 'home.html', {})
+	return render(request, 'home.html')
 
 def about(request):
-	return render(request, 'about.html', {})
+	return render(request, 'about.html')
 
 def blog(request):
-	return render(request, 'blog.html', {})
+	return render(request, 'blog.html')
 
 def covers(request):
-	return render(request, 'covers.html', {})
+	return render(request, 'covers.html')
