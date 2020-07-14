6df39d87b40f9690bcb86d2a545d2cc6b4702245
V2E <ENGINE@V-Engine.local>
2020-07-14 13:26:28 -0700

    aff url edit

--- projwsite/aff/views.py
+++ projwsite/aff/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse,request
 
 # Create your views here.
 def home(request):
-	return render(request, 'home.html')
+	return render(request, 'home.html', {})
 
 def about(request):
 	return render(request, 'about.html')
