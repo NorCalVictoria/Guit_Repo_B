d14ea2d5840c7b9687959ea3c779282c671f10ca
V2E <ENGINE@V-Engine.local>
2020-07-13 22:47:20 -0700

    about and blog templates added

--- projwsite/aff/views.py
+++ projwsite/aff/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
 	return render(request, 'home.html', {})
+
+def about(request):
+	return render(request, 'about.html', {})
+
+def blog(request):
+	return render(request, 'blog.html', {})
+
+def covers(request):
+	return render(request, 'covers.html', {})
