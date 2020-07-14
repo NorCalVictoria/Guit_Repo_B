57824e4a350ac3f56dbf922404e4374697d27366
V2E <ENGINE@V-Engine.local>
2020-07-14 13:28:22 -0700

    aff url edit

--- projwsite/projwsite/urls.py
+++ projwsite/projwsite/urls.py
@@ -5,7 +5,7 @@ from aff import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('aff', include('aff.urls')),
+    path('aff/', include('aff.urls')),
 
     path('', views.home, name='home'),
 	path('about/', views.about, name='about'),
