d5dc20494678151a0d3c28b611a7ae4b7c3cdec6
V2E <ENGINE@V-Engine.local>
2020-07-19 18:58:17 -0700

    added landing page

--- projwsite/aff/urls.py
+++ projwsite/aff/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, reverse
 from . import views
 urlpatterns = [
 	path('', views.home, name='home'),
+	path('landing/', views.land, name='land'),
+	path('home/', views.home,name='home'),
 	path('about/', views.about, name='about'),
 	path('blog/', views.blog, name='blog'),
 	path('covers/', views.covers, name='covers'),
