b01038556bfa78302a0113ca9a3d82bb38440ac9
V2E <ENGINE@V-Engine.local>
2020-08-21 00:19:37 -0700

    db postgreSQL setup

--- projwsite/projwsite/urls.py
+++ projwsite/projwsite/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path, include
 from aff import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
@@ -13,4 +15,4 @@ urlpatterns = [
 	path('about/', views.about, name='about'),
 	path('blog/', views.blog, name='blog'),
 	path('covers/', views.covers, name='covers'),
-]
+] +static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
